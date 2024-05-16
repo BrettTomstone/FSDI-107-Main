825ea3769ae5f88f5e88031f995b45a525cc6947
BrettTomstone <163075325+BrettTomstone@users.noreply.github.com>
2024-05-15 20:19:44 -0400

    Add files via upload

---
+++ FSDI 107/intro.py
@@ -0,0 +1,44 @@
+# Numeric Variables
+age = 25
+height = 5.8
+price = 9.99
+
+# String Variables
+name = "Brett Thompson"
+address = "1851 Knollwood Dr."
+email = "brettdthompson@live.com"
+
+# Boolean Variables
+is_student = True
+has_license = False
+
+# List Variables
+fruits = ["apple", "banana", "orange"]
+numbers = [1, 2, 3, 4, 5]
+
+# Dictionary Variables
+person = {
+    "name": "Brett",
+    "age": 36,
+    "city": "Tampa"
+}
+
+# Printing the variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Price:", price)
+print("Address:", address)
+print("Email:", email)
+print("Is Student:", is_student)
+print("Has License:", has_license)
+print("Fruits:", fruits)
+print("Numbers:", numbers)
+print("Person:", person)
+
+# Accessing specific elements in lists and dictionaries
+print("First Fruit:", fruits[0])
+print("Third Number:", numbers[2])
+print("Person's City:", person["city"])
+
+
